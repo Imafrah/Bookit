de4fbdf20dd5325b20305d6b2401adc378703754
Imafrah <imafrah03@gmail.com>
2025-10-31 23:26:16 +0530

    Added /api/health route

--- backend/src/app.js
+++ backend/src/app.js
@@ -23,7 +23,7 @@ db.authenticate()
 
 app.use('/api/experiences', experienceRoutes);
 app.use('/api/bookings', bookingRoutes);
-app.use('/api/promo', promoRoutes);
+app.use('/api/promocodes', promoRoutes);
 
 app.get('/', (req, res) => {
   res.json({ status: "ok" });
